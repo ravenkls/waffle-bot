f00bb15b6d3610de17a5cbb17210dc78839caa03
Kristian <kristian.smith.2001@hotmail.co.uk>
2020-04-02 17:55:08 +0100

    token in file

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 from discord.ext import commands
 
+with open("token.txt") as f:
+    token = f.read()
 
 bot = commands.Bot(command_prefix="?")
 bot.load_extension("general")
-bot.run("MzU4OTU0Mzg3OTUzMDI1MDI0.XoVLsA.brKZIwOu-62JHIQNcw2qdRe596k")
+bot.run(token)
